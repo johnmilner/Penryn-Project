79b73b56a0e379c4b54be5634889cc6aafa95dfd
johnmilner <john.milner@mac.com>
2018-04-25 23:51:14 -0700

    update background color gradient for header and complete migration from new-portfolio

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -10751,6 +10751,53 @@ var intro = function intro() {
   tl.play();
 };
 
+anime.timeline({ loop: false }).add({
+  targets: ".ml8 .circle-white",
+  scale: [0, 3],
+  opacity: [1, 0],
+  easing: "easeInOutExpo",
+  rotateZ: 360,
+  duration: 1100,
+  delay: 1000
+}).add({
+  targets: ".ml8 .circle-container",
+  scale: [0, 1],
+  duration: 1100,
+  easing: "easeInOutExpo",
+  offset: "-=1000"
+}).add({
+  targets: ".ml8 .circle-dark",
+  scale: [0, 1],
+  duration: 1100,
+  easing: "easeOutExpo",
+  offset: "-=600"
+}).add({
+  targets: ".ml8 .letters-left",
+  scale: [0, 1],
+  duration: 1200,
+  offset: "-=550"
+}).add({
+  targets: ".ml8 .bang",
+  scale: [0, 1],
+  rotateZ: [45, 15],
+  duration: 1200,
+  offset: "-=1000"
+}).add({
+  targets: ".ml8",
+  opacity: 0,
+  duration: 2000,
+  easing: "easeOutExpo",
+  delay: 300
+});
+
+anime({
+  targets: ".ml8 .circle-dark-dashed",
+  rotateZ: 360,
+  duration: 5000,
+  easing: "linear",
+  loop: true
+});
+
 var Loader = function Loader() {
   var preloaderFadeOutTime = 2500;
   function hidePreloader() {
@@ -11078,52 +11125,6 @@ var AboutController = function () {
         key: 'outro',
         value: function outro() {
             Transition.outro({
-                listeners: Listeners
-            });
-        }
-    }]);
-    return AboutController;
-}();
-
-/*
-
-router.init('/', HomeController)
-router.init('/about', AboutController)
-router.init('/work/:id/:name', WorkOneController).with('id', '[0-9]+').with('name', '[a-z0-9-]+')
-router.init('/work/:type', WorkAllController).with('type', 'date|title')
-router.init('/work', WorkAllController)
-
-*/
-
-var Route = function Route() {
-    classCallCheck(this, Route);
-
-    var router = new Router();
-
-    router.init('/', HomeController);
-    router.init('/about', AboutController);
-
-    router.error(ErrorController);
-
-    router.run();
-};
-
-var App = function App() {
-    classCallCheck(this, App);
-
-    window.Penryn = window.Penryn || {};
-
-    Support.init();
-
-    skylake.TopWhenRefresh();
-
-    new Route();
-};
-
-(function (_) {
-  return new App();
-})();
-{
                 listeners: Listeners
             });
         }
