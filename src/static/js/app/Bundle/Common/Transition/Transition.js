00c043118e238de945061ef3e706ecb7fc51b5ea
johnmilner <john.milner@mac.com>
2018-05-10 20:55:19 -0700

    implement header transition offscreen and onscreen on mousewheel pt.2

--- src/static/js/app/Bundle/Common/Transition/Transition.js
+++ src/static/js/app/Bundle/Common/Transition/Transition.js
@@ -1,6 +1,8 @@
 /* eslint-disable */
 
 import S from 'skylake'
+import jQuery from "jquery";
+
 
 const Transition = {}
 
@@ -57,31 +59,81 @@ var eleHeader = null;
 
     Transition.headerUp = new S.Timeline()
     const isObj3 = S.Is.object(Transition.headerUp)
-    Transition.headerUp.from({el: '.header', p: {y: [0, -100]}, d: 1600, e: 'Power4InOut', delay: 500})
+    Transition.headerUp.from({el: '.header', p: {y: [0, -100]}, d: 1300, e: 'ExpoOut'})
+    // Transition.headerUp.from({el: '.header', p: {y: [-50, -100]}, d: 800, e: 'ExpoOut'})
+
+    // Transition.headerUp.from(".header", "3dy", -100, -50, 500, "Power4In"), e.from("#.header", "3dy", -50, 0, 800, "ExpoOut", {
+    //     delay: 500
+    //    })
     
 
 
 
     Transition.headerDown = new S.Timeline()
     const isObj4 = S.Is.object(Transition.headerDown)
-    Transition.headerDown.from({el: '.header', p: {y: [-100, 0]}, d: 1600, e: 'Power4InOut', delay: 500})
+    Transition.headerDown.from({el: '.header', p: {y: [-100, 0]}, d: 1300, e: 'ExpoOut'})
     
     
 
 window.onload = function() {
 
-window.addEventListener('wheel', function(e) {
-    if (e.deltaY < 0) {
-        console.log('scrolling up');
-        Transition.headerDown.play()
-        // document.getElementById('status').innerHTML = 'scrolling up';
+    var isMoving = false;
+
+    window.addEventListener('wheel', function(e) {
+        if (isMoving) return;
+        if (e.deltaY < 0) {
+          console.log('scrolling up');
+          Transition.headerUp.play({delay: 500})
+        }
+        if (e.deltaY > 0) {
+          console.log('scrolling down');
+          Transition.headerDown.play({delay: 500})        
+        }
+        navigateTo();
+      });
+    // $('body').bind("mousewheel DOMMouseScroll MozMousePixelScroll", function(event, delta) {
+    //    event.preventDefault();
+    //    //if (isMoving) return;
+    // //    if (event.deltaY > 0) {
+    // //     Transition.headerDown.play({delay: 500})
+    // //    } else {
+    // //     Transition.headerUp.play({delay: 500})
+    // //    }
+    // if (event.deltaY < 0) {
+    //     console.log('scrolling up');
+    //     Transition.headerDown.play({delay: 500}) 
+          
+    // }
+    //   if (event.deltaY > 0) {
+    //     console.log('scrolling down');
+    //     Transition.headerUp.play({delay: 500})   
+          
+    // }
+    //    //!isMoving ? Transition.headerUp.play({delay: 500}) : Transition.headerDown.play()
+    //    if (isMoving) return;
+    //    navigateTo();
+    //    console.log('scrolling')
+    // });
+    
+    function navigateTo(){
+       isMoving = true;
+       setTimeout(function() {
+        isMoving = false;
+       },2000);
     }
-    if (e.deltaY > 0) {
-        console.log('scrolling down');
-        Transition.headerUp.play()
-        // document.getElementById('status').innerHTML = 'scrolling down';
-    }
-    });
+
+// window.addEventListener('wheel', function(e) {
+//     if (e.deltaY < 0) {
+//         console.log('scrolling up');
+//         Transition.headerDown.play()
+//         // document.getElementById('status').innerHTML = 'scrolling up';
+//     }
+//     if (e.deltaY > 0) {
+//         console.log('scrolling down');
+//         Transition.headerUp.play()
+//         // document.getElementById('status').innerHTML = 'scrolling down';
+//     }
+//     });
 
 // const eleHeader = document.querySelector('.header');
 // console.log(eleHeader);
